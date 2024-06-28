f8e7fbb2830566311f220273b8427f594ab91314
farandd <152785029+farandd@users.noreply.github.com>
2024-06-28 22:52:11 +0700

    Update discorit.c

--- discorit.c
+++ discorit.c
@@ -3,109 +3,208 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <pthread.h>
 
-#define PORT 6000
-#define BUFFER_SIZE 1024
+#define PORT 8080
+#define IP "127.0.0.1"
 
-int server_fd;
-char logged_in_username[50] = {0};
-pthread_mutex_t print_mutex = PTHREAD_MUTEX_INITIALIZER;
+void register_user(int sock, const char *username, const char *password) {
+    char buffer[1024];
+    sprintf(buffer, "REGISTER %s -p %s", username, password);
+    send(sock, buffer, strlen(buffer), 0);
+    memset(buffer, 0, sizeof(buffer));
+    read(sock, buffer, sizeof(buffer));
+    printf("%s\n", buffer);
+}
 
-void connect_to_server() {
+void create_room(int sock, const char *room) {
+    char buffer[1024];
+    sprintf(buffer, "CREATE ROOM %s", room);
+    send(sock, buffer, strlen(buffer), 0);
+    memset(buffer, 0, sizeof(buffer));
+    read(sock, buffer, sizeof(buffer));
+    printf("%s\n", buffer);
+}
+
+void edit_room(int sock, const char *old_room, const char *new_room) {
+    char buffer[1024];
+    sprintf(buffer, "EDIT ROOM %s TO %s", old_room, new_room);
+    send(sock, buffer, strlen(buffer), 0);
+    memset(buffer, 0, sizeof(buffer));
+    read(sock, buffer, sizeof(buffer));
+    printf("%s\n", buffer);
+}
+
+void delete_room(int sock, const char *room) {
+    char buffer[1024];
+    sprintf(buffer, "DEL ROOM %s", room);
+    send(sock, buffer, strlen(buffer), 0);
+    memset(buffer, 0, sizeof(buffer));
+    read(sock, buffer, sizeof(buffer));
+    printf("%s\n", buffer);
+}
+
+void delete_all_rooms(int sock) {
+    char buffer[1024];
+    sprintf(buffer, "DEL ROOM ALL");
+    send(sock, buffer, strlen(buffer), 0);
+    memset(buffer, 0, sizeof(buffer));
+    read(sock, buffer, sizeof(buffer));
+    printf("%s\n", buffer);
+}
+
+void login_user(int sock, const char *username, const char *password) {
+    char buffer[1024];
+    sprintf(buffer, "LOGIN %s -p %s", username, password);
+    send(sock, buffer, strlen(buffer), 0);
+    memset(buffer, 0, sizeof(buffer));
+    read(sock, buffer, sizeof(buffer));
+    printf("%s\n", buffer);
+
+    // Check if login was successful
+    if (strstr(buffer, "username berhasil login") != NULL) {
+        char current_username[50];
+        strcpy(current_username, username);
+
+        char channel[50] = "";
+        char room[50] = "";
+
+        while (1) {
+            if (strlen(room) > 0) {
+                printf("[%s/%s/%s] ", current_username, channel, room);
+            } else if (strlen(channel) > 0) {
+                printf("[%s/%s] ", current_username, channel);
+            } else {
+                printf("[%s] ", current_username);
+            }
+            
+            fgets(buffer, sizeof(buffer), stdin);
+            buffer[strcspn(buffer, "\n")] = 0; // Remove newline character
+
+            send(sock, buffer, strlen(buffer), 0);
+            memset(buffer, 0, sizeof(buffer));
+            read(sock, buffer, sizeof(buffer));
+            printf("%s\n", buffer);
+
+            // Check if the response contains "Profil diupdate menjadi"
+            if (strstr(buffer, "Profil diupdate menjadi") != NULL) {
+                strtok(buffer, " "); // Skip "Profil"
+                strtok(NULL, " "); // Skip "diupdate"
+                strtok(NULL, " "); // Skip "menjadi"
+                char *new_username = strtok(NULL, "\n"); // Get the new username
+                strcpy(current_username, new_username);
+            } 
+        
+        if (strstr(buffer, "CREATE ROOM") != NULL) {
+                char *room = strtok(buffer + 12, "\n"); // Get room name after "CREATE ROOM "
+                create_room(sock, room);
+            } else if (strstr(buffer, "EDIT ROOM") != NULL) {
+                char *old_room = strtok(buffer + 10, " "); // Get old room name after "EDIT ROOM "
+                strtok(NULL, " "); // Skip "TO"
+                char *new_room = strtok(NULL, "\n"); // Get new room name after "TO"
+                edit_room(sock, old_room, new_room);
+            } else if (strstr(buffer, "DEL ROOM ALL") != NULL) {
+                delete_all_rooms(sock);
+            } else if (strstr(buffer, "DEL ROOM") != NULL) {
+                char *room = strtok(buffer + 9, "\n"); // Get room name after "DEL ROOM "
+                delete_room(sock, room);
+            } else if (strcmp(buffer, "Exited room\n") == 0) {
+                // Server responded with "Exited room", so clear room variable
+                strcpy(room, "");
+            } else if (strcmp(buffer, "Exited channel\n") == 0) {
+                // Server responded with "Exited channel", so clear channel and room variables
+                strcpy(channel, "");
+                strcpy(room, "");
+            } else if (strcmp(buffer, "Exited\n") == 0) {
+                // Server responded with "Exited", so break the loop and exit the program
+                break;
+            } else {
+                if (strstr(buffer, "joined the channel") != NULL) {
+                // Skip the first part "username joined the channel"
+                strtok(buffer, " "); // Skip username
+                strtok(NULL, " "); // Skip "joined"
+                strtok(NULL, " "); // Skip "the"
+                strtok(NULL, " "); // Skip "channel"
+                char *joined_channel = strtok(NULL, "\n"); // Get the channel name
+                strcpy(channel, joined_channel); // Update the channel variable
+                strcpy(room, ""); // Reset room name
+            } else if (strstr(buffer, "joined the room") != NULL) {
+                // Skip the first part "username joined the room"
+                strtok(buffer, " "); // Skip username
+                strtok(NULL, " "); // Skip "joined"
+                strtok(NULL, " "); // Skip "the"
+                strtok(NULL, " "); // Skip "room"
+                char *joined_room = strtok(NULL, "\n"); // Get the room name
+                strcpy(room, joined_room); // Update the room variable
+            } else if (strstr(buffer, "butuh key") != NULL) {
+                printf("Key: ");
+                char key[50];
+                fgets(key, sizeof(key), stdin);
+                key[strcspn(key, "\n")] = 0; // Remove newline character
+
+                sprintf(buffer, "%s", key);
+                send(sock, buffer, strlen(buffer), 0);
+                memset(buffer, 0, sizeof(buffer));
+                read(sock, buffer, sizeof(buffer));
+                printf("%s\n", buffer);
+
+                if (strstr(buffer, "joined the channel") != NULL) {
+                    // Skip the first part "username joined the channel"
+                    strtok(buffer, " "); // Skip username
+                    strtok(NULL, " "); // Skip "joined"
+                    strtok(NULL, " "); // Skip "the"
+                    strtok(NULL, " "); // Skip "channel"
+                    char *joined_channel = strtok(NULL, "\n"); // Get the channel name
+                    strcpy(channel, joined_channel); // Update the channel variable
+                    strcpy(room, ""); // Reset room name
+                }
+          }
+        }
+
+        }
+    }
+}
+
+
+
+int main(int argc, char *argv[]) {
+    if (argc < 4) {
+        fprintf(stderr, "Usage: %s REGISTER|LOGIN username -p password\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    int sock = 0;
     struct sockaddr_in serv_addr;
+    char buffer[1024] = {0};
 
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("Socket creation error");
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Socket creation failed");
         exit(EXIT_FAILURE);
     }
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
 
-    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+    if (inet_pton(AF_INET, IP, &serv_addr.sin_addr) <= 0) {
         perror("Invalid address/ Address not supported");
         exit(EXIT_FAILURE);
     }
 
-    if (connect(server_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        perror("Connection Failed");
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Connection failed");
         exit(EXIT_FAILURE);
     }
-}
 
-void *receive_handler(void *arg) {
-    char buffer[BUFFER_SIZE];
-    while (1) {
-        memset(buffer, 0, BUFFER_SIZE);
-        int recv_size = recv(server_fd, buffer, BUFFER_SIZE - 1, 0);
-        if (recv_size <= 0) {
-            pthread_mutex_lock(&print_mutex);
-            printf("Disconnected from server.\n");
-            pthread_mutex_unlock(&print_mutex);
-            exit(0);
-        }
-        pthread_mutex_lock(&print_mutex);
-        printf("%s", buffer);
-        if (strcmp(buffer, "Disconnected from server.\n") != 0) {
-            printf("[%s] ", logged_in_username);
-        }
-        fflush(stdout);
-        pthread_mutex_unlock(&print_mutex);
-    }
-    return NULL;
-}
+    printf("Connected to server\n");
 
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        printf("Usage: %s <command> [args...]\n", argv[0]);
-        return 1;
-    }
-
-    connect_to_server();
-
-    char command[BUFFER_SIZE] = {0};
-    for (int i = 1; i < argc; i++) {
-        strcat(command, argv[i]);
-        if (i < argc - 1) strcat(command, " ");
-    }
-
-    send(server_fd, command, strlen(command), 0);
-
-    char response[BUFFER_SIZE] = {0};
-    recv(server_fd, response, BUFFER_SIZE - 1, 0);
-
-    if (strncmp(command, "LOGIN", 5) == 0) {
-        if (strstr(response, "berhasil login") != NULL) {
-            sscanf(response, "%s berhasil login", logged_in_username);
-            printf("[%s] ", logged_in_username);
-            fflush(stdout);
-
-            pthread_t recv_thread;
-            pthread_create(&recv_thread, NULL, receive_handler, NULL);
-
-            while (1) {
-                char input[BUFFER_SIZE] = {0};
-                if (fgets(input, BUFFER_SIZE, stdin) == NULL) break;
-                input[strcspn(input, "\n")] = 0;
-
-                if (strcmp(input, "QUIT") == 0) {
-                    send(server_fd, input, strlen(input), 0);
-                    break;
-                }
-
-                send(server_fd, input, strlen(input), 0);
-            }
-
-            pthread_cancel(recv_thread);
-            pthread_join(recv_thread, NULL);
-        } else {
-            printf("%s", response);
-        }
+    if (strcmp(argv[1], "REGISTER") == 0) {
+        register_user(sock, argv[2], argv[4]);
+    } else if (strcmp(argv[1], "LOGIN") == 0) {
+        login_user(sock, argv[2], argv[4]);
     } else {
-        printf("%s", response);
+        fprintf(stderr, "Invalid command\n");
+        exit(EXIT_FAILURE);
     }
 
-    close(server_fd);
+    close(sock);
     return 0;
 }
